05f96103aa2d3375a6ca028829f5c975b3c86170
Bob Pham <bobpham.tdp@gmail.com>
2026-06-02 22:55:09 +1000

    remove unogranized projects

--- BrokerSolution/KafkaConsumer1/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using KafkaConsumer1.Configuration;
-using KafkaConsumer1.Services;
-
-var settings = AppConfiguration.Load();
-await KafkaConsumerService.ConsumeAsync(settings);
--- BrokerSolution/KafkaConsumer2/Handlers/IKafkaMessageHandler.cs
+++
@@ -1,8 +0,0 @@
-using Confluent.Kafka;
-
-namespace KafkaConsumer2.Handlers;
-
-public interface IKafkaMessageHandler
-{
-    Task HandleAsync(ConsumeResult<Ignore, string> result, CancellationToken cancellationToken);
-}
